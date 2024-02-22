b2d63fbda2a8adf9746dba97ff3bf2d7572d7996
Justin9091 <jusraaijmakers@gmail.com>
2024-02-22 10:49:38 +0100

    Added short url

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -9,6 +9,15 @@
         <button class="{{$button}}">Exporteer registratie</button>
     @endif
 
+    <h1 class="text-3xl">Short url</h1>
+    <p>Mooie text, moet toch nog vertaald worden</p>
+
+    <x-form action="/shorturl/edit" method="POST">
+        <x-text-input name="short_url" placeholder="New url" />
+
+        <x-submit-button />
+    </x-form>
+
 
     <h1 class="text-3xl">Favorites</h1>
 
