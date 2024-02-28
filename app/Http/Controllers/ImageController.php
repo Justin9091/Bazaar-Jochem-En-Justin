8774890da6511f6fbe4f860ab4bbed36d602bd9f
Justin9091 <jusraaijmakers@gmail.com>
2024-02-28 09:32:43 +0100

    Remove old image if present

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ImageRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
@@ -16,13 +18,26 @@ class ImageController extends Controller
         $extension = $file->getClientOriginalExtension(); // Get the original file extension
         $fileName = $folder . '/' . $name . '.' . $extension;
 
-        if(Storage::disk('public')->exists($fileName)) {
-            Storage::disk('public')->delete($fileName);
-        }
+        $this->removeOldImage(Auth::user()->id);
 
         Storage::disk('public')->put($fileName, file_get_contents($file));
 
 
         return back();
+    }
+
+    private function removeOldImage($id)
+    {
+        $logos = Storage::disk('public')->files('logos');
+        foreach ($logos as $key => $logo) {
+            $fullName = File::basename($logo);
+
+            $nameWithoutExtension = substr($fullName, 0, strrpos($fullName,'.'));
+
+            if($nameWithoutExtension == $id) {
+                Storage::disk('public')->delete('logos/' . $fullName);
+                break;
+            }
+        }
     }
 }
