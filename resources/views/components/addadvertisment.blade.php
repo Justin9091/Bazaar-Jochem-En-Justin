2c52c61c7d3abd62d76a38f84353157adb2bb3ef
Ducky1804 <128805195+Ducky1804@users.noreply.github.com>
2024-03-14 13:09:01 +0100

    styling

--- resources/views/components/addadvertisment.blade.php
+++ resources/views/components/addadvertisment.blade.php
@@ -1,4 +1,2 @@
-@if(Auth::check() && Auth::id() == $userid)
-    <a href="{{ route('sellers.addadvertisement', ['userId' => $userid]) }}" class="block py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-300 ease-in-out">Aanbieding toevoegen</a>
-    <p class="text-gray-500">Logged in as: {{ auth()->id() }}</p>
-@endif
+<a href="{{ route('sellers.addadvertisement', ['userId' => $userid]) }}" class="block py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-300 ease-in-out">Aanbieding toevoegen</a>
+
