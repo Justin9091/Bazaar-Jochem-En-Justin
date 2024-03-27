9122f41d75f0dbb303fe530c67988861c250d505
Justin9091 <jusraaijmakers@gmail.com>
2024-03-27 10:16:24 +0100

    Fixed styling

--- resources/views/components/form/text-input.blade.php
+++ resources/views/components/form/text-input.blade.php
@@ -1,5 +1,5 @@
 <div class="p-2 w-full">
-    <input type="{{$type}}" placeholder="{{$placeholder}}" name="{{$name}}" class="p-2 w-full rounded-lg"
+    <input type="{{$type}}" placeholder="{{$placeholder}}" name="{{$name}}" class="p-2 w-full rounded-lg text-black"
            value="{{$value}}"/>
     <p class="text-red-500">{{ $errors->first($name) }}</p>
 </div>
