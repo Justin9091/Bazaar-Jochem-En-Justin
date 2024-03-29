65e8b75df40e61d2b73a28e49089611ae17cd982
Justin9091 <jusraaijmakers@gmail.com>
2024-03-29 12:43:12 +0100

    Boemie

--- resources/views/contract.blade.php
+++ resources/views/contract.blade.php
@@ -6,12 +6,12 @@
 
     <div class="bg-gray-800 rounded-lg flex flex-col items-center justify-center p-5">
 
-        <h1 class="text-2xl font-bold mb-4">{{ __('contract.contractpage') }}</h1>
+        <h1 class="text-2xl font-bold mb-4">{{ __('contract.title') }}</h1>
         <x-contract userid="{{$userid}}"></x-contract>
 
         @if(!empty($contracts))
         <div class="container mx-auto">
-            <h1 class="text-2xl font-bold mb-4">{{ __('contract.contracts') }}</h1>
+            <h1 class="text-2xl font-bold mb-4">{{ __('contract.title') }}</h1>
             <ul>
                 @foreach ($contracts as $contract)
                     <li>
